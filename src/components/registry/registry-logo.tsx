ac88a4f3d327b31647a6c8b1ac82b42ab953b037
willsather <will@sather.ws>
2025-06-05 10:05:10 -0400

    theme: add default shadcn theme tokens

--- src/components/registry/registry-logo.tsx
+++ src/components/registry/registry-logo.tsx
@@ -1,10 +1,17 @@
-import { Squirrel } from "lucide-react";
-
 export function RegistryLogo() {
   return (
     <>
-      <div className="flex-shrink-0 rounded-md bg-primary p-1">
-        <Squirrel className="size-5 text-secondary" />
+      <div className="flex-shrink-0 rounded bg-primary p-2">
+        <svg
+          width="76"
+          height="65"
+          viewBox="0 0 76 65"
+          xmlns="http://www.w3.org/2000/svg"
+          role="graphics-symbol"
+          className="size-4 fill-white dark:fill-black"
+        >
+          <path d="M37.5274 0L75.0548 65H0L37.5274 0Z" />
+        </svg>
       </div>
       <span className="font-semibold">Registry</span>
     </>
